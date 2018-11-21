01f6844c44e4883f1330b73fff3615c92f99e3b5
Rief Haalboom <superrieff@hotmail.com>
2018-11-21 16:58:49 +0100

    test scene camera stuff

---
+++ Project 3.2/Assets/[Source]/Scenes/Rief/TestScripts/IslandCamera.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class IslandCamera : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera buildingCam;
+    public CinemachineFreeLook mainCam;
+    [SerializeField] private GameObject infoCanvas;
+
+    void Start()
+    {
+        
+    }
+
+    void Update ()
+    {
+        if (Input.GetButtonDown ("Cancel"))
+        {
+            mainCam.enabled = true;
+            buildingCam.enabled = false;
+            infoCanvas.SetActive (false);
+        }
+    }
+
+    void OnMouseDown ()
+    {
+        mainCam.enabled = false;
+        buildingCam.enabled = true;
+        infoCanvas.SetActive (true);
+    }
+}
