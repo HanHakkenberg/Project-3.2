967358ca59b3f7a9d2c717d6a03f0590b12f1497
han hakkenberg <han.hakkenberg@gmail.com>
2018-11-30 12:00:24 +0100

    Navigation

--- Project 3.2/Assets/[Source]/Scripts/Core/Ship/MapInteractable.cs
+++ Project 3.2/Assets/[Source]/Scripts/Core/Ship/MapInteractable.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapInteractable{
-    
+public class MapInteractable : MonoBehaviour{
+    [SerializeField] TransformReference currentSelected;
+
+    void OnMouseDown() {
+        if(!Input.GetButton("Waypoint Interact")) {
+            currentSelected.Value = null;
+        }
+    }
+
 }
---
+++ Project 3.2/Assets/[Source]/Scripts/Core/Utilities/Scriptable Variables/LayerMask/LayerMaskVariable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "LayerMask", menuName = "Variable/LayerMask")]
+public class LayerMaskVariable : ScriptableObject {
+    [Multiline][SerializeField] string variableInfo;
+
+    [SerializeField] LayerMask baseValue;
+    [SerializeField] LayerMask value;
+
+    [SerializeField] GameEvent[] myEvent;
+    [SerializeField] bool reset = true;
+
+    public LayerMask Value {
+        get {
+            return value;
+        }
+        set {
+            this.value = value;
+
+            if (myEvent != null) {
+                for(int i = 0; i < myEvent.Length; i++) {
+                    myEvent[i].Raise();
+                }
+            }
+        }
+    }
+
+    public void OnEnable() {
+        if (reset == true) {
+            value = baseValue;
+        }
+    }
+
+    public void ManualReset() {
+        if (reset == true) {
+            value = baseValue;
+        }
+    }
+}
