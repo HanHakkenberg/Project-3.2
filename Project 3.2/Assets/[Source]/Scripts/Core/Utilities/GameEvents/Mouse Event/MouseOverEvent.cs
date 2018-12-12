12f56592419a890be19646bbfd75bd2f87eb3447
han hakkenberg <han.hakkenberg@gmail.com>
2018-12-12 16:33:25 +0100

    Main Menu Base

---
+++ Project 3.2/Assets/[Source]/Scripts/Core/Utilities/GameEvents/Mouse Event/MouseOverEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseOverEvent : MonoBehaviour {
+    [SerializeField] GameEvent[] myEvent;
+    [SerializeField] UnityEvent myUnityEvent;
+
+    void OnMouseOver() {
+        for (int i = 0; i < myEvent.Length; i++) {
+            myEvent[i].Raise();
+        }
+
+        myUnityEvent.Invoke();
+    }
+}
