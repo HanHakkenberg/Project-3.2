4d1d23e856a6329df1027ab8f5d9da2f160dc233
han hakkenberg <han.hakkenberg@gmail.com>
2018-12-21 11:50:36 +0100

    Switch Buttons

--- Project 3.2/Assets/[Source]/Scripts/Core/Map/ShipSwitch.cs
+++ Project 3.2/Assets/[Source]/Scripts/Core/Map/ShipSwitch.cs
@@ -2,4 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipSwitch : MonoBehaviour {}
+public class ShipSwitch : MonoBehaviour {
+    public static ShipSwitch instance;
+
+    public List<ShipSwitchButton> ships = new List<ShipSwitchButton>();
+    [SerializeField] int shipAmount;
+    [SerializeField] GameObject shipSwitchButton;
+
+    void Awake() {
+        for (int i = 0; i < shipAmount; i++) {
+            GameObject button = Instantiate(shipSwitchButton, new Vector3(), new Quaternion(), transform);
+
+            ShipSwitchButton newButton = button.GetComponent<ShipSwitchButton>();
+
+            newButton.myIndex = i;
+
+            ships.Add(newButton);
+        }
+    }
+
+    public void SopttedObject(int index) {
+
+        for (int i = 0; i < ships.Count; i++) {
+            if (ships[i].myIndex == index) {
+                ships[i].spottingImage.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    public void Interactable(int index, bool State) {
+        for (int i = 0; i < ships.Count; i++) {
+            if (ships[i].myIndex == index) {
+                ships[i].spottingImage.SetActive(State);
+                break;
+            }
+        }
+    }
+
+    public void AddShip(Transform shipTransform) {
+        for (int i = 0; i < ships.Count; i++) {
+            if (ships[i].myShip == null) {
+                ships[i].myShip = shipTransform;
+                return;
+            }
+        }
+
+        Debug.LogError("Not Enough Buttons For Ships!");
+    }
+
+    public void RemoveShip(Transform toRemove) {
+        for (int i = 0; i < ships.Count; i++) {
+            if (ships[i].myShip == toRemove) {
+                ships[i].myShip = null;
+                return;
+            }
+        }
+
+    }
+
+    public void RemoveShip(int toRemoveIndex) {
+        ships[toRemoveIndex] = null;
+    }
+}
---
+++ Project 3.2/Assets/[Source]/Scripts/Core/Map/ShipSwitchButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSwitchButton : MonoBehaviour {
+    public Transform myShip;
+    public GameObject spottingImage;
+    public GameObject interactButton;
+    public int myIndex;
+
+    public void PressButton() {
+        spottingImage.SetActive(false);
+        interactButton.SetActive(false);
+    }
+}
