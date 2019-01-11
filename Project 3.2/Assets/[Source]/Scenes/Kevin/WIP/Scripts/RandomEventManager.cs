2b952022319ff994e3998b23914bd5110f471ac8
Kevin Otten <kevin.otten@ziggo.nl>
2019-01-11 11:46:04 +0100

    begin events

---
+++ Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/RandomEventManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEventManager : MonoBehaviour
+{
+    public List<ScriptableEvent> randomEventsList = new List<ScriptableEvent>();
+    public List<ScriptableEvent> EventsList = new List<ScriptableEvent>();
+
+    public void TriggerRandomEvent()
+    {
+        
+    }
+
+    // public void TriggerEvent(ScriptableEvent)
+    // {
+
+    // }
+
+}
---
+++ Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/ScriptableEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu(fileName = "Event", menuName = "ScriptableEvent", order = 1)]
+public class ScriptableEvent : ScriptableObject
+{
+    
+    public List<ResourceDropdown> affedctedResources = new List<ResourceDropdown>();
+    public TMP_Text Title;
+    public TMP_Text Message;
+}
+
+public class ResourceDropdown
+{
+    CivManager.Type ResoureType;
+    int Value;
+}
