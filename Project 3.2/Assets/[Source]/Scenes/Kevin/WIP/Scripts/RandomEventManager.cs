dc752586636e55d764ec405847be1911b687f93a
Kevin Otten <kevin.otten@ziggo.nl>
2019-01-13 20:06:52 +0100

    Event shit en UI hiervoor

--- Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/RandomEventManager.cs
+++ Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/RandomEventManager.cs
@@ -1,20 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RandomEventManager : MonoBehaviour
 {
     public List<ScriptableEvent> randomEventsList = new List<ScriptableEvent>();
     public List<ScriptableEvent> EventsList = new List<ScriptableEvent>();
+    ScriptableEvent activeEvent;
+    public GameObject button;
+    public Transform buttonLocation;
+    public TMP_Text eventText;
+    public TMP_Text eventTitle;
 
+
+    /// <summary>
+    /// Call this function to have a chance of triggering a random event
+    /// </summary>
     public void TriggerRandomEvent()
     {
-        
+        // Using the Chance int combined with the if statement gives the random event a chance to trigger instead of a guarantee
+        int Chance = Random.Range(1,11);
+        if (Chance > 8)
+        {
+            int eventIndex = Random.Range(0,randomEventsList.Count);
+            activeEvent = randomEventsList[eventIndex];
+            PlayEvent(activeEvent);
+        }
     }
 
-    // public void TriggerEvent(ScriptableEvent)
-    // {
-
-    // }
-
+    /// <summary>
+    /// Call this function whit the event you want to activate
+    /// </summary>
+    /// <param name="curentEvent">The event you want to activate</param>
+    public void PlayEvent(ScriptableEvent curentEvent)
+    {
+        eventText.text = curentEvent.Message;
+        eventTitle.text = curentEvent.Title;
+        for (int i = 0; i < curentEvent.eventOptions.Count; i++)
+        {
+            Instantiate(button,buttonLocation.position,Quaternion.identity);
+        }
+    }
 }
--- Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/ScriptableEvent.cs
+++ Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/ScriptableEvent.cs
@@ -6,16 +6,24 @@ using TMPro;
 [CreateAssetMenu(fileName = "Event", menuName = "Event/ScriptableEvent", order = 1)]
 public class ScriptableEvent : ScriptableObject
 {
-    
-    public List<ResourceDropdown> affectedResources = new List<ResourceDropdown>();
+    public List<EventOptions> eventOptions = new List<EventOptions>();
     public string Title;
     [Multiline]
     public string Message;
 }
 
 [System.Serializable]
-public class ResourceDropdown
+public class EventOptions
 {
+    public List<EventOptionsEffects> eventOptionsEffects = new List<EventOptionsEffects>();
+    public string buttonText;
+}
+
+[System.Serializable]
+public class EventOptionsEffects
+{
+    //This class contains all the effects a Events options performs
+
     [Tooltip("Type of resource changed by event")]
     public CivManager.Type ResoureType;
     [Tooltip("Value changed by event")]
