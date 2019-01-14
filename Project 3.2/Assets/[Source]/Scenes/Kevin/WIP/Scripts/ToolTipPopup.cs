3603ce79cc1cfc1881145f479ee0aa606f0f3253
Kevin Otten <kevin.otten@ziggo.nl>
2019-01-14 17:04:20 +0100

    hover over shit begin

--- Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/ToolTipPopup.cs
+++ Project 3.2/Assets/[Source]/Scenes/Kevin/WIP/Scripts/ToolTipPopup.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class ToolTipPopup : MonoBehaviour
 {
-    
+    public GameObject toolTipPannel;
+    public static Transform toolTip;
+    public static string toolTipText;
+
+    void Update() 
+    {
+        toolTip.position = Input.mousePosition;    
+    }
+
+    void OnMouseEnter() 
+    {
+        toolTipPannel.SetActive(true);
+    }
+
+    void OnMouseExit() 
+    {
+        toolTipPannel.SetActive(false);        
+    }
 }
