cbb5f599828fc85defb7473b4b8e487bda4d8ce2
han hakkenberg <han.hakkenberg@gmail.com>
2019-01-17 12:19:20 +0100

    Menu

---
+++ Project 3.2/Assets/[Source]/Scripts/Core/Utilities/GameEvents/Mouse Event/MouseClickEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseClickEvent : MonoBehaviour {
+    [SerializeField] GameEvent[] myEvent;
+    [SerializeField] UnityEvent myUnityEvent;
+
+    void OnMouseDown() {
+        myUnityEvent.Invoke();
+
+        for (int i = 0; i < myEvent.Length; i++) {
+            myEvent[i].Raise();
+        }
+    }
+
+}
