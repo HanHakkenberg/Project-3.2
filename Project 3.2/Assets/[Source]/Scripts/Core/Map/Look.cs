ee491653e7988ef33291efea3478f2a16a07c03b
han hakkenberg <han.hakkenberg@gmail.com>
2019-01-21 16:07:47 +0100

    Post Prosessing

---
+++ Project 3.2/Assets/[Source]/Scripts/Core/Map/Look.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour {
+    [SerializeField] TransformReference mainCam;
+
+    void Update() {
+        transform.LookAt(mainCam.Value);
+    }
+}
