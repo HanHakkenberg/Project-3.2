663f9e4aa987c805da1f1f006c0f9434d63dba03
han hakkenberg <han.hakkenberg@gmail.com>
2019-01-23 15:13:43 +0100

    Menu

---
+++ Project 3.2/Assets/StartFade.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartFade : MonoBehaviour {
+    [SerializeField] Image target;
+    [SerializeField] float speed;
+    [SerializeField] bool fadeIn;
+    [SerializeField] AudioSource audio;
+
+    void Update() {
+        if(fadeIn) {
+            if(target.color.a > 0.95f) {
+                Application.Quit();
+            }
+
+            target.color = new Vector4(0, 0, 0, Mathf.Lerp(target.color.a, 1, speed * Time.deltaTime));
+        }
+        else {
+            if(target.color.a > 0.95f) {
+                SceneManager.LoadScene("Main");
+            }
+
+            target.color = new Vector4(0, 0, 0, Mathf.Lerp(target.color.a, 1, speed * Time.deltaTime));
+        }
+
+        audio.volume = Mathf.Lerp(audio.volume, 0, speed * Time.deltaTime);
+
+    }
+
+    public void SetFade(bool fadIn) {
+        fadeIn = fadIn;
+    }
+}
