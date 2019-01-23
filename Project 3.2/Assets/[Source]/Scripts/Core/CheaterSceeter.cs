1241a076c5dd94d53868f9f1cf59b800448949e7
Kevin Otten <kevin.otten@ziggo.nl>
2019-01-23 15:57:52 +0100

    cheetersheeter

---
+++ Project 3.2/Assets/[Source]/Scripts/Core/CheaterSceeter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheaterSceeter : MonoBehaviour
+{
+    void Update() 
+    {
+        UpdateCheat();
+    }
+
+    void UpdateCheat()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            print("test");
+            AddResourcesCheat();
+        }
+        if(Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            RemoveResourcesCheat();
+        }
+        if(Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            TriggerEventCheat();
+        }
+    }
+
+    void AddResourcesCheat()
+    {
+        CivManager.instance.AddIncome(100,CivManager.Type.Food);
+        CivManager.instance.AddIncome(100,CivManager.Type.Money);
+        CivManager.instance.AddIncome(100,CivManager.Type.Mats);
+    }
+
+    void RemoveResourcesCheat()
+    {
+        CivManager.instance.RemoveIncome(100,CivManager.Type.Food);
+        CivManager.instance.RemoveIncome(100,CivManager.Type.Money);
+        CivManager.instance.RemoveIncome(100,CivManager.Type.Mats);
+    }
+
+    void TriggerEventCheat()
+    {
+        EventManager.instance.TriggerRandomEvent();
+    }
+}
